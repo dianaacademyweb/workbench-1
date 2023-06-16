840672e08f51e51ae06d8c655767910c5f0335ea
Ashish <ashishrohilla510@gmail.com>
2023-06-17 00:31:37 +0530

    adding company login details

--- Frontend/src/components/LoginPage/CompanyLogin.jsx
+++ Frontend/src/components/LoginPage/CompanyLogin.jsx
@@ -7,6 +7,10 @@ function CompanyLogin() {
             <input type="text" name="username" placeholder="Enter username" className="text-black" />
             <input type="password" name="password" placeholder="enter the password"  className="text-black"/>
             <input type="submit" />
+
+            <h1>
+              this is the company login page you can do it any thing there 
+            </h1>
     
     </form>
     </div>
