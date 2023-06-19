17d66a842a9f185d46247e2ba4f7ef8559e2b293
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-06-19 22:54:31 +0530

    adding home page

--- Frontend/src/components/NewHome.jsx
+++ Frontend/src/components/NewHome.jsx
@@ -26,15 +26,15 @@ const NewHome = () => {
       </nav>
 
 
-    {/* navigation bar ended     */}
+    {/* navigation bar ended */}
       <div className="flex  items-center justify-center">
       <h2 className="text-3xl  mb-4 text-white font-arial-rounded">WELCOME TO DIANA EMPLOYMENT PORTAL</h2>
       </div>
       <div className="flex  text-white items-center justify-center tracking-custom-wide text-lg font-poppins">A TEAM WORK'S TOGETHER, STAY'S TOGETHER AND ACHIVE GREATNESS TOGETHER.</div>
       <div className="flex flex-col sm:flex-row mt-4 m-4 justify-between text-white text-lg">
-        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl  border-gray-200"><div className="inner-box font-calibri  bg-cyan-700 p-2 hover:mt-4 hover:mb-4 hover:ml-4  h-full overflow-hidden">Our employee management software helps you efficiently manage your workforce, automate tasks, and boost productivity. With our intuitive interface, you can easily create and manage teams, add employees, and organize them according to departments, projects, or roles.</div></div>
-        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl  border-gray-200"><div className="inner-box font-calibri  bg-cyan-700 p-2 hover:mt-4 hover:mb-4 hover:ml-4  h-full overflow-hidden">Simplify the employee onboarding process by centralizing all employee data in one place. Track employee progress, manage training programs, and monitor performance evaluations. Our software also allows you to assign projects, tasks, and deadlines to individual employees or teams, ensuring efficient project management.</div></div>
-        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl  border-gray-200"><div className="inner-box font-calibri  bg-cyan-700 p-2 hover:mt-4 hover:mb-4 hover:ml-4  h-full overflow-hidden">Faster collaboration within your organization with built-in communication and collaboration tools. Employees can exchange messages, share files, and collaborate on projects directly within the software. Stay updated with real-time notifications, task progress, and project status. Additionally, our software provides comprehensive reporting and analytics, giving you insights into employee performance, project timelines, and resource allocation.</div></div>
+        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl border border-gray-200"><div className="inner-box text-center font-calibri  bg-cyan-700 p-2   h-full overflow-hidden">Our employee management software helps you efficiently manage your workforce, automate tasks, and boost productivity. With our intuitive interface, you can easily create and manage teams, add employees, and organize them according to departments, projects, or roles</div></div>
+        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl border border-gray-200"><div className="inner-box text-center font-calibri  bg-cyan-700 p-2   h-full overflow-hidden">Simplify the employee onboarding process by centralizing all employee data in one place. Track employee progress, manage training programs, and monitor performance evaluations. Our software also allows you to assign projects, tasks, and deadlines to individual employees or teams, ensuring efficient project management</div></div>
+        <div className="box  w-1/3 bg-gray-200 h-80 mr-20 ml-20 shadow-2xl border border-gray-200"><div className="inner-box text-center font-calibri  bg-cyan-700 p-2   h-full overflow-hidden">Faster collaboration within your organization with built-in communication and collaboration tools. Employees can exchange messages, share files, and collaborate on projects directly within the software. Stay updated with real-time notifications, task progress, and project status. Additionally, our software provides comprehensive reporting </div></div>
       </div>
 
 
