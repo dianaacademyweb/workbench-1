648e3a9097c7e77dfd6d4aafecf6131b72e3a412
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-08-09 23:50:19 +0530

    new calender

--- Frontend/src/EmployeeDash/EmployeeDash.jsx
+++ Frontend/src/EmployeeDash/EmployeeDash.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import dashgroup from "../assets/Images/layouts/dashgroup.jpg";
-import blur from "../assets/Images/layouts/blur.jpg";
-import pro from "../assets/Images/layouts/pro.jpg";
-import { FiSettings, FiCheckCircle } from "react-icons/fi";
-import { AiOutlineClockCircle } from "react-icons/ai";
-import Calendar from "react-calendar";
+
+
 // import '/src/calendar.css'
 import Notifications from "./Notifications";
+import Example from "./Calender";
 const EmployeeDash = () => {
 
   const notifications = [
@@ -18,6 +16,12 @@ const EmployeeDash = () => {
     },
     {
       id: 2,
+      title: 'Meeting Reminder',
+      description: 'Don\'t forget the team meeting at 3:00 PM today.',
+      date: '2023-07-28',
+    },
+    {
+      id: 3,
       title: 'Meeting Reminder',
       description: 'Don\'t forget the team meeting at 3:00 PM today.',
       date: '2023-07-28',
@@ -51,21 +55,21 @@ const EmployeeDash = () => {
     // Add more bulletin items as needed
   ];
   return (
-    <div className="min-h-screen bg-navy-800">
+    <div className="min-h-screen ">
       <div className="md:flex">
         <div className="flex flex-col">
           <div
-            className="flex justify-between p-4 h-[450px]  bg-cover  max-h-screen "
+            className="flex sm:flex-row flex-col justify-between p-4 h-[450px]  bg-cover  max-h-screen "
             style={{ backgroundImage: `url(${dashgroup})` }}
           >
-            <div className="bg-navy-500 text-lightPrimary bg-opacity-40 p-2 w-1/4 h-1/4 shadow-md rounded-md mb-4">
+            <div className="bg-navy-500 text-lightPrimary bg-opacity-40 p-2 md:w-1/4 h-1/4 shadow-md rounded-md mb-4">
               <h2 className="text-xl font-bold mb-2">Location and Time</h2>
               <div>
                 <p className=" mb-1">Location: {currentLocation}</p>
                 <p className=" mb-1">Time: {currentTime}</p>
               </div>
             </div>
-            <div className="bg-navy-500 text-lightPrimary flex flex-col  p-2 shadow-md rounded-md mb-4 h-1/2 w-1/3 bg-opacity-30">
+            <div className="bg-navy-500 text-lightPrimary flex flex-col  p-2 shadow-md rounded-md mb-4 md:h-1/2 md:w-2/5 bg-opacity-30">
               <h2 className="text-xl font-bold mb-2">My Logs</h2>
               <ul className="list-disc  text-md">
                 {logs.map((log) => (
@@ -77,21 +81,21 @@ const EmployeeDash = () => {
             </div>
            
           </div>
-          <div className="md:flex gap-4 mt-30">
+          <div className="md:flex gap-1 mt-30">
             <div className="bg-gradient-to-r from-blue-100 to-purple-100 p-4 shadow-md rounded-md mb-4">
               <h2 className="text-2xl font-bold mb-4 text-blue-900">Bulletin Board</h2>
               <ul className="list-none">
                 {bulletinItems.map((item) => (
                   <li key={item.id} className="mb-4">
                     <h3 className="text-lg font-bold  text-blue-900">{item.title}</h3>
-                    <p className="text-gray-600 mb-1">Date: {item.date}</p>
-                    <p className="text-gray-800">{item.description}</p>
+                    <p className="text-navy-500 mb-1">Date: {item.date}</p>
+                    <p className="text-navy-800">{item.description}</p>
                   </li>
                 ))}
               </ul>
             </div>
-            <div className="bg-gradient-to-r from-blue-100 to-purple-100 md:w-1/3 p-4 shadow-md rounded-md mb-4">
-              <Calendar />
+            <div className="bg-gradient-to-r from-blue-100 to-purple-100  p-4 shadow-md rounded-md mb-4">
+              <Example/>
             </div>
             <div>
             <div>
